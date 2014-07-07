a5658e49684e1921fb0f1b9faa335613a74a683d
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-07-07 11:48:37 -0400

    Add postmark settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'freeshelf.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => ENV["POSTMARK_SMTP_SERVER"],
+                                         :user_name => ENV['POSTMARK_API_KEY'],
+                                         :password => ENV['POSTMARK_API_KEY'],
+                                         :authentication => :plain}
 end
