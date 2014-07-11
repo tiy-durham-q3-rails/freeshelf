bba23632cd6e0c1e733ac761473041992a7fff71
michaelbyrd <michaelbyrdsemail@gmail.com>
2014-07-11 15:22:43 -0400

    renamed my migration

--- db/migrate/20140711184902_add_suggestion_list_last_looked_at_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSuggestionListLastLookedAtUsers < ActiveRecord::Migration
-  def change
-    add_column :users,:last_looked, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711184902) do
+ActiveRecord::Schema.define(version: 20140711192000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20140711184902) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "last_looked"
+    t.datetime "last_looked_at_suggestions"
   end
 
 end
