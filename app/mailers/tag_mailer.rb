11c88d698308d9bf4eeb40baab1f81a9db8ab154
Erin Brown <emberbrown@gmail.com>
2014-07-11 22:42:15 -0400

    Edited mailer model and views
    *Added html and plain text email templates
    *Created a email method to send these emails

--- app/mailers/tag_mailer.rb
+++ app/mailers/tag_mailer.rb
@@ -1,3 +1,11 @@
 class TagMailer < ActionMailer::Base
   default from: "freeshelf@freeshelf.com"
+
+  def new_tag_alert(user, slug, tag)
+    @user = user
+    @slug = slug
+    @tag = tag
+    @url = "http://www.freeshelf.herokuapp.com/books/#{slug}"
+    mail(to: @user.email, subject: "A New Book Was Added With Your Favorited Tag!")
+  end
 end
