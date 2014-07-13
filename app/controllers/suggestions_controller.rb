a4a535b5551d4ca17ae567340f4e565483a6b9b0
michaelbyrd <michaelbyrdsemail@gmail.com>
2014-07-13 09:34:29 -0400

    forgot to uncomment

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,15 +3,15 @@ class SuggestionsController < ApplicationController
     @suggestions = find_user_suggestions
   end
 
-  # def find_user_suggestions
-  #   suggest = []
-  #   current_user.favorite_tags.each do |t|
-  #     Book.all.each do |b|
-  #       if b.tags.include?(t) && b.updated_at < DateTime.now
-  #         suggest << b
-  #       end # if
-  #     end # each recently_update_book
-  #   end # each favorite_tag
-  #   suggest
-  # end
+  def find_user_suggestions
+    suggest = []
+    current_user.favorite_tags.each do |t|
+      Book.all.each do |b|
+        if b.tags.include?(t) && b.updated_at < DateTime.now
+          suggest << b
+        end # if
+      end # each recently_update_book
+    end # each favorite_tag
+    suggest
+  end
 end
