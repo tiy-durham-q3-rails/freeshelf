e889514fc9ca2b7742b2c721673b4892e5620984
michaelbyrd <michaelbyrdsemail@gmail.com>
2014-07-13 09:31:54 -0400

    somehow I can no longer favorite a tag

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,19 +1,17 @@
 class SuggestionsController < ApplicationController
   def index
-    find_user_suggestions
+    @suggestions = find_user_suggestions
   end
 
-  def find_user_suggestions
-    @suggestions = []
-    current_user.favorite_books.each do |b|
-      if b.updated_at < (DateTime.now) #(current_user.last_looked_at_suggestions)
-        @suggestions << b
-      end
-    end
-  end
-
-  def possible_suggestions
-    @possible_suggestions = []
-
-  end
+  # def find_user_suggestions
+  #   suggest = []
+  #   current_user.favorite_tags.each do |t|
+  #     Book.all.each do |b|
+  #       if b.tags.include?(t) && b.updated_at < DateTime.now
+  #         suggest << b
+  #       end # if
+  #     end # each recently_update_book
+  #   end # each favorite_tag
+  #   suggest
+  # end
 end
