d549e0affbfe187ec9ae0af38042f49ff2336ab7
michaelbyrd <michaelbyrdsemail@gmail.com>
2014-07-13 11:39:34 -0400

    issues with updating suggestions

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,18 +5,20 @@ class SuggestionsController < ApplicationController
 
   def find_user_suggestions
     suggest = []
-    current_user.favorite_tags.each do |t|
-      Book.all.each do |b|
-        if b.tags.include?(t) && (b.updated_at.to_i > current_user.last_looked_at_suggestions.to_i)
-          suggest << b
+    h = Hash.new(0)
+
+    current_user.favorite_tags.each do |tag|
+      Book.all.each do |book|
+        if (current_user.last_looked_at_suggestions < book.updated_at) && book.tags.include?(tag)
+          suggest << book
         end # if
       end # each recently_update_book
     end # each favorite_tag
-    h = Hash.new(0)
+
     suggest.each{ |e| h[e] += 1 }
     sort = h.sort{|a,b| a[1] <=> b[1]}
     suggest = sort.collect { |k, v| k }
-    # current_user.last_looked_at_suggestions = DateTime.now
+    current_user.last_looked_at_suggestions = DateTime.now
     suggest.reverse
   end # find_user_suggestions
 end
