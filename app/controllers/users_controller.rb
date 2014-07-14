a6849bf7324e2aa6b8c9360841a1fbc1585c9095
Sebastian Armano <sebarmano@gmail.com>
2014-07-13 23:56:43 -0400

    Added translations to new views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signing up!"
+      redirect_to root_url, notice: t(:thank_you)
     else
       render :new
     end
